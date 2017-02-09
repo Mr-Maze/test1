7c8057eb09af42f69d60ccbaa0d20efb676fe485
Mads <madsgjerrildsoegaard@gmail.com>
2017-02-09 10:47:14 +0100

    1

--- jeglaverkode.py
+++ jeglaverkode.py
@@ -1,5 +1,12 @@
 #-*- coding: utf-8 -*-
 
-print "sup"
-print "yøæåoyoyo"
-#print "det virker jo fint"
+mit_navn = 'Mads'
+alder = 25
+hojde = 185
+
+print "lad os snakke lidt om %s." % mit_navn
+print "han er %d år gammel" % alder
+print "Han har en super højde på %d cm" % hojde
+
+#nu til det svære
+print "hvis vi ligger %d, %d sammen, så får vi %d" %(alder, hojde, alder + hojde)
